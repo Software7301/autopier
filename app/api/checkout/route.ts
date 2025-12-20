be0558ff612e497f96a0962fa639235ecd861738
Software7301 <software7301@users.noreply.github.com>
2025-12-19 23:43:23 -0300

    Sincronizar schema do Prisma com banco de dados

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
       carId,
       customerName,
       customerRg,
-      customerPhone,
+      customerPhone,        
       paymentMethod,
       installments,
       totalPrice,
